97443b3c724da11765ae548ff9fd3a898b3a1de0
Kishore D R <kishore328@gmail.com>
2020-10-12 12:54:07 +0530

    router is updatede

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ const cors = require("cors")
 const app = express()
 const port = process.env.PORT || 3005
 
+
 app.use(express.json())
 app.use(cors())
+
+app.get('/test', (req, res) => {
+    res.send('test Hello World!')
+  })
+
 app.use("/",function (req,res){
-    res.send("Hello World !!!")
+    res.send("Hello World  Node")
 })
 
 app.listen(port,function(){
